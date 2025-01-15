df58e7ede3d98d0c62e974a2d940d30e86b0dca3
Rhythunand <rhythunandbiju@gmail.com>
2025-01-15 21:14:56 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,5 @@
+attendance = float(input("Enter your attendance percentage : "))
+if attendance >= 75.0 :
+  print("You are eligible to write the exam")
+else :
+  print("You are not eligible to write the exam")
